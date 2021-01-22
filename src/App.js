ee30b69e99adf76c88bb50a2e35983fa5b1ae3e0
simow <you@example.com>
2021-01-22 22:45:39 +0100

    movie trailers

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import './App.css';
 import React from 'react'; 
 import Row from './Row'
 import requests from './Requests';
-import Banner from './Banner'
+import Banner from './Banner';
+import Nav from './Nav';
 
 
 function App() {
   return (
     <div className="app">
-      <Banner  />
+      <Nav/>
+      <Banner/>
       <Row title = "Netflix originals" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title = "Trending now" fetchUrl={requests.fetchTrending} />
       <Row title = "Top rated" fetchUrl={requests.fetchTopRated} />
+      <Row title = "Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title = "Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title = "Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title = "Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title = "Documentaries" fetchUrl={requests.fetchDocumentaries} />
 
     </div>
   );
--- src/Requests.js
+++ src/Requests.js
@@ -4,6 +4,12 @@ const requests = {
     fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en_US`,
     fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
     fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en_US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&width_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&width_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&width_genres=28`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&width_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&width_genres=99`,
+
 }
 
 export default requests;
